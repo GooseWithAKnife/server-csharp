7305f6de0d7c5c09da88c6d0df8a2adc68fb3b0c
Alex <alex@dm-me-for-questions.com>
2025-06-03 16:05:54 +0100

    double nope

--- Libraries/FodyWeavers/JsonExtensionData.Fody/TypeProcessor.cs
+++ Libraries/FodyWeavers/JsonExtensionData.Fody/TypeProcessor.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
 using FieldAttributes = Mono.Cecil.FieldAttributes;
 using MethodAttributes = Mono.Cecil.MethodAttributes;
 using ParameterAttributes = Mono.Cecil.ParameterAttributes;
@@ -13,8 +15,9 @@ namespace JsonExtensionData.Fody;
 public partial class ModuleWeaver
 {
     private TypeReference? _dictionaryStringObjectReference;
+    // private MethodReference? _dictionaryStringObjectConstructorReference;
     private MethodReference? _jsonExtensionDataAttributeReference;
-    private MethodReference? _jsonIgnoreAttributeReference;
+
     public void ProcessType(TypeDefinition typeDefinition)
     {
         if (typeDefinition.BaseType != null && typeDefinition.BaseType.FullName != "System.Object")
@@ -23,12 +26,33 @@ public partial class ModuleWeaver
         }
 
         // If the property already has somewhere on the inheritance chain the JsonExtensionDataAttribute, dont add it again
-        if (PropertiesFlattened(typeDefinition).Any(p => p.CustomAttributes.Any(a => a.AttributeType.FullName.Contains("JsonExtensionDataAttribute"))))
+        if (FieldsFlattened(typeDefinition).Any(f =>
+                f.CustomAttributes.Any(a => a.AttributeType.FullName.Contains("JsonExtensionDataAttribute"))))
         {
             return;
         }
 
-        _dictionaryStringObjectReference ??= ModuleDefinition.ImportReference(typeof(Dictionary<string, object>));
+        if (_dictionaryStringObjectReference == null)
+        {
+            var mscorlibModule = AssemblyResolver.Resolve(AssemblyNameReference.Parse("mscorlib, Version=4.0.0.0")).MainModule;
+
+            var dictionaryDefinition = new GenericInstanceType(mscorlibModule.ExportedTypes.Single(t => t.FullName == "System.Collections.Generic.Dictionary`2").Resolve());
+            var stringReference = mscorlibModule.ExportedTypes.Single(t => t.FullName == "System.String").Resolve();
+            var objectReference = mscorlibModule.ExportedTypes.Single(t => t.FullName == "System.Object").Resolve();
+            dictionaryDefinition.GenericArguments.Add(stringReference);
+            dictionaryDefinition.GenericArguments.Add(objectReference);
+
+            /*
+            var constructor = new GenericInstanceMethod(dictionaryDefinition.Resolve().Methods.First(m => m.IsConstructor && !m.HasParameters));
+            constructor.GenericArguments.Add(stringReference);
+            constructor.GenericArguments.Add(objectReference);
+            constructor.GenericParameters.Add(new GenericParameter(stringReference));
+            constructor.GenericParameters.Add(new GenericParameter(objectReference));
+            _dictionaryStringObjectConstructorReference = ModuleDefinition.ImportReference(constructor);
+            */
+            _dictionaryStringObjectReference = ModuleDefinition.ImportReference(dictionaryDefinition);
+        }
+
         if (_jsonExtensionDataAttributeReference is null)
         {
             var jsonConstructorReference = ModuleDefinition.AssemblyResolver
@@ -37,14 +61,7 @@ public partial class ModuleWeaver
                 .First(m => m.IsConstructor && !m.HasParameters);
             _jsonExtensionDataAttributeReference = ModuleDefinition.ImportReference(jsonConstructorReference);
         }
-        if (_jsonIgnoreAttributeReference is null)
-        {
-            var jsonIgnoreConstructorReference = ModuleDefinition.AssemblyResolver
-                .Resolve(AssemblyNameReference.Parse("System.Text.Json")).MainModule
-                .GetType("System.Text.Json.Serialization.JsonIgnoreAttribute").Methods
-                .First(m => m.IsConstructor && !m.HasParameters);
-            _jsonIgnoreAttributeReference = ModuleDefinition.ImportReference(jsonIgnoreConstructorReference);
-        }
+
         var propertyDefinition = new PropertyDefinition("ExtensionData", PropertyAttributes.None, _dictionaryStringObjectReference);
         propertyDefinition.CustomAttributes.Add(new CustomAttribute(_jsonExtensionDataAttributeReference));
 
@@ -52,7 +69,6 @@ public partial class ModuleWeaver
         var field = new FieldDefinition("_extensionData",
             FieldAttributes.Private,
             _dictionaryStringObjectReference);
-        field.CustomAttributes.Add(new CustomAttribute(_jsonIgnoreAttributeReference));
         typeDefinition.Fields.Add(field);
 
         // Add getter
@@ -76,26 +92,15 @@ public partial class ModuleWeaver
         set.Body.Instructions.Add(Instruction.Create(OpCodes.Stfld, field));
         set.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
 
-        foreach (var constructor in typeDefinition.Methods.Where(m => m.IsConstructor && !m.IsStatic))
-        {
-            var processor = constructor.Body.GetILProcessor();
-            var ldArg0 = Instruction.Create(OpCodes.Ldarg_0);
-            processor.InsertBefore(constructor.Body.Instructions.First(), ldArg0);
-            var newInstruction = processor.Create(OpCodes.Newobj, ModuleDefinition.ImportReference(typeof(Dictionary<string, object>).GetConstructor(Type.EmptyTypes)));
-            processor.InsertAfter(ldArg0, newInstruction);
-            var setField = processor.Create(OpCodes.Stfld, field);
-            processor.InsertAfter(newInstruction, setField);
-        }
-
         propertyDefinition.SetMethod = set;
         typeDefinition.Methods.Add(set);
         typeDefinition.Properties.Add(propertyDefinition);
     }
 
-    public static List<PropertyDefinition> PropertiesFlattened(TypeDefinition asmType)
+    public static List<FieldDefinition> FieldsFlattened(TypeDefinition asmType)
     {
         //get properties on main type:
-        var props = new List<PropertyDefinition>(asmType.Properties.Select(property => property));
+        var fields = new List<FieldDefinition>(asmType.Fields.Select(field => field));
 
         //get properties of base types:
         if (asmType.BaseType != null && asmType.BaseType.FullName != "System.Object")
@@ -104,9 +109,9 @@ public partial class ModuleWeaver
 
             //recursive call:
             if (baseType != null)
-                props.AddRange(PropertiesFlattened(baseType));
+                fields.AddRange(FieldsFlattened(baseType));
         }
 
-        return props;
+        return fields;
     }
 }
